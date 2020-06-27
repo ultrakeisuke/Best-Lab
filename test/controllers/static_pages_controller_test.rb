7ced661fc3f647efa6c1302ae28472abcabdf7e8
ultrakeisuke <aucwm605@gmail.com>
2020-06-27 14:36:41 +0900

    Add User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get static_pages_url
+    get root_path
     assert_response :success
   end
 
