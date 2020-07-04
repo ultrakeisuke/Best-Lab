70300483fa169cc022845b3e153f3845c61fbb27
ultrakeisuke <aucwm605@gmail.com>
2020-07-05 08:35:32 +0900

    Add devise controllers and edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  root to:'static_pages#home'
+  root 'static_pages#home'
   get  '/help', to:'static_pages#help'
   get  '/about', to:'static_pages#about'
   get  '/contact', to:'static_pages#contact'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+  devise_scope :user do
+    get "sign_in", to:"users/sessions#new"
+    get "sign_out", to:"users/sessions#destroy"
+  end
+  get  'users/:id', to:'users#show'
 end
