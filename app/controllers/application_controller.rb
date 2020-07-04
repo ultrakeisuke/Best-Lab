3078e3e113c1d407d78540c79a1ce9a75e6fb1d5
ultrakeisuke <aucwm605@gmail.com>
2020-07-05 08:35:32 +0900

    Add devise controllers and edit routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
 end
+
+# class Users::ApplicationController < ApplicationController
+# before_action :authenticate_user!
+# end
