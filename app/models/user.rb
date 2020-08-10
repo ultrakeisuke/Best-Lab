596b9dc48d9377a31556699e6e17b338bbbed2ad
ultrakeisuke <aucwm605@gmail.com>
2020-08-10 17:20:31 +0900

    Add: affiliationカラムに大学生を表すundergraduate:0と、大学院生を表すgraduate:1を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   mount_uploader :picture, PictureUploader
   validates :name, presence: true, length: { maximum: 50 }
-  validates :affiliation, inclusion: { in: [true, false] }
+  enum  affiliation: { undergraduate: 0, graduate: 1 }
   validates :profile, length: { maximum: 200 }
 
 
