fcd1f9690b396588a2ede7cabe4a01da9503933b
ultrakeisuke <aucwm605@gmail.com>
2020-08-10 17:08:01 +0900

    Fix: 所属を決めるカラム(affiliation)の値が必ず存在するよう設定

--- db/migrate/20200810075626_add_affiliation_to_users.rb
+++ db/migrate/20200810075626_add_affiliation_to_users.rb
@@ -1,5 +1,5 @@
 class AddAffiliationToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :affiliation, :boolean
+    add_column :users, :affiliation, :integer, null: false, default: 0
   end
 end
