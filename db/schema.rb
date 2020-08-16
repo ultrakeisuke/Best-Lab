bea9505a5849283f432c427d82ab19a742830e02
ultrakeisuke <aucwm605@gmail.com>
2020-08-16 12:37:39 +0900

    Add: nameカラムにnull:falseを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2020_08_16_031107) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -23,7 +24,6 @@ ActiveRecord::Schema.define(version: 2020_08_16_031107) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
