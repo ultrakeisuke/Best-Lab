08c38f57a216162e439a862846d6062809efe375
ultrakeisuke <aucwm605@gmail.com>
2020-08-17 07:17:23 +0900

    Add: Adminを認証するconfirmable関連のカラムを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,18 @@ ActiveRecord::Schema.define(version: 2020_08_16_031107) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -48,8 +52,8 @@ ActiveRecord::Schema.define(version: 2020_08_16_031107) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
-    t.boolean "admin", default: false
     t.integer "affiliation", default: 0, null: false
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
