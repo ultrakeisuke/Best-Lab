a2b21761444acc2fada8b0b9787467c84cc3dea1
ultrakeisuke <aucwm605@gmail.com>
2020-08-22 04:09:09 +0900

    Fix: 管理者だけがすべてのユーザー情報にアクセスできるように修正。

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Admins::UsersController < ApplicationController
+  before_action :authenticate_admin!
 
   def index
     @users = User.all.page(params[:page])
