8a9e2ab2ea94b048786f715628d6885ce665092f
ultrakeisuke <aucwm605@gmail.com>
2020-08-22 05:31:23 +0900

    Remove: nameカラムを削除。

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
-  validates :name, presence: true, length: { maximum: 50 }
-
+         
 end
