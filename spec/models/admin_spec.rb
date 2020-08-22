7ed2d2a474b7b5168e4f2de4e259a3acb0ff3824
ultrakeisuke <aucwm605@gmail.com>
2020-08-22 10:42:01 +0900

    Add: adminデータが有効であるテストを追加。

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin) { create(:admin) }
+
+  it "FactoryBotでadminデータが有効" do
+    expect(admin).to be_valid
+  end
 end
