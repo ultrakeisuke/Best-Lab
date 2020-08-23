ee54144f0a4155613171f198ff1fb93157c0f632
ultrakeisuke <aucwm605@gmail.com>
2020-08-23 15:19:28 +0900

    Fix: protectedメソッドのインデントを修正。一つ右に移動。

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -18,14 +18,14 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   protected
 
-  # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+    # The path used after resending confirmation instructions.
+    # def after_resending_confirmation_instructions_path_for(resource_name)
+    #   super(resource_name)
+    # end
 
-  # The path used after confirmation.
-  def after_confirmation_path_for(resource_name, resource)
-    super(resource_name, resource)
-  end
+    # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      super(resource_name, resource)
+    end
 
 end
