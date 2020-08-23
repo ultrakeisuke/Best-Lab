4efa27ae446b493f223399c78eaac4592502ec2f
ultrakeisuke <aucwm605@gmail.com>
2020-08-23 15:19:28 +0900

    Fix: protectedメソッドのインデントを修正。一つ右に移動。

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -20,17 +20,17 @@ class Admins::SessionsController < Devise::SessionsController
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_in_params
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    # end
 
-  def after_sign_in_path_for(resource)
-    admins_users_path
-  end
+    def after_sign_in_path_for(resource)
+      admins_users_path
+    end
 
-  def after_sign_out_path_for(resource)
-    about_path
-  end
+    def after_sign_out_path_for(resource)
+      about_path
+    end
 
 end
