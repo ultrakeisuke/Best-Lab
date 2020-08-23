9dee9e4a96118f8764ab75bc2eeb8fa01cb2d8ac
ultrakeisuke <aucwm605@gmail.com>
2020-08-23 15:16:52 +0900

    Fix: すべてのアクションをコメントインに修正。Deviseコントローラから継承した処理を変更していないため。

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,21 +3,26 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :sign_in_params, only: [:create]
 
-  def new
-    super
-  end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
-  def create
-    super
-  end
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
-  def destroy
-    super
-  end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
 
   protected
 
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:emai])
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_in_params
+    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    # end
+
 end
