053462eec17f14c0900b9b68ff730456371aa99c
ultrakeisuke <aucwm605@gmail.com>
2020-08-25 07:34:59 +0900

    Add: before_action :configure_sign_in_paramsを追加。
    メールアドレスをストロングパラメータに加え、createアクションを対象に入れました。

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admins::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -21,9 +21,9 @@ class Admins::SessionsController < Devise::SessionsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
 
   def after_sign_in_path_for(resource)
     admins_users_path
