b6e175e938a1cbfc99b3300c267b436a63d0b888
ultrakeisuke <aucwm605@gmail.com>
2020-08-26 21:11:58 +0900

    Update: カバレッジレポートを正常に出力するよう修正。
    require 'simplecov'を最初に読み込まれるよう配置しました。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# カバレッジを把握するためのgem(simplecov)を設定
+require 'simplecov'
+SimpleCov.start 'rails'
+
 RSpec.configure do |config|
 
   config.expect_with :rspec do |expectations|
@@ -19,10 +23,6 @@ RSpec.configure do |config|
   config.before(:all) do
     FactoryBot.reload
   end
-
-  # カバレッジを把握するためのgem(simplecov)を設定
-  require 'simplecov'
-  SimpleCov.start 'rails'
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
