05519a97e508b9c5bfb08e5e12f904656b728de0
ultrakeisuke <aucwm605@gmail.com>
2020-08-29 17:59:40 +0900

    Clean: 管理者ログインの記述をリファクタリング。
    ベタ書きからlogin_adminメソッドを使った記述に修正しました。

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -35,20 +35,14 @@ RSpec.feature 'ユーザー情報の閲覧と削除' do
   end
 
   scenario 'ユーザー情報を閲覧する' do
-    visit new_admin_session_path
-    fill_in 'メールアドレス', with: admin.email
-    fill_in 'パスワード', with: '1234567'
-    click_button 'ログイン'
+    login_admin(admin)
     expect(page).to have_content 'すべてのユーザー'
     click_link 'user'
     expect(page).to have_selector 'h3', text: 'userさんのプロフィール'
   end
 
   scenario 'ユーザーアカウントを削除する' do
-    visit new_admin_session_path
-    fill_in 'メールアドレス', with: admin.email
-    fill_in 'パスワード', with: '1234567'
-    click_button 'ログイン'
+    login_admin(admin)
     expect(page).to have_content 'すべてのユーザー'
     click_link 'user'
     expect{ click_button 'アカウントの削除' }.to change(User, :count).by(-1)
@@ -68,11 +62,7 @@ RSpec.feature 'ページネーション表示' do
   end
   
   scenario 'ページごとのユーザー数とページネーションリンクを正常に表示する' do
-    visit new_admin_session_path
-    fill_in 'メールアドレス', with: admin.email
-    fill_in 'パスワード', with: '1234567'
-    click_button 'ログイン'
-
+    login_admin(admin)
     expect(all('li').size).to eq(25)
     expect(page).to have_selector 'a', text: '次'
     expect(page).to have_selector 'a', text: '最後'
