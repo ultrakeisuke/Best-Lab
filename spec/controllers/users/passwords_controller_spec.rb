f36d08e0d5b071444ee739203cd1846c355d3856
ultrakeisuke <aucwm605@gmail.com>
2020-08-31 06:26:57 +0900

    Clean: ユーザーの承認に関する記述をリファクタリング。
    user.confirmをbefore内に定義しました。

--- spec/controllers/users/passwords_controller_spec.rb
+++ spec/controllers/users/passwords_controller_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Users::PasswordsController, type: :controller do
   let(:user) { create(:user) }
+  before do
+    user.confirm 
+  end
 
   describe "createアクション" do
     context "パスワード変更のメール送信に失敗する場合" do
       it "メール送信画面にレンダリングする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        user.confirm
         post :create, params: { user: { email: "" } }
         expect(response).to have_http_status "200"
         expect(response).to render_template :new
@@ -16,7 +18,6 @@ RSpec.describe Users::PasswordsController, type: :controller do
     context "パスワード変更のメール送信に成功する場合" do
       it "ログイン画面にリダイレクトする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        user.confirm
         post :create, params: { user: { email: "user@example.com" } }
         expect(response).to have_http_status "302"
         expect(assigns(:user)).to eq user
@@ -28,7 +29,6 @@ RSpec.describe Users::PasswordsController, type: :controller do
     context "パスワード再設定に失敗する場合" do
       it "パスワード入力画面にレンダリングする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        user.confirm
         put :update, params: { user: { password: "", password_confirmation: "" } }
         expect(response).to have_http_status "200"
         expect(response).to render_template :edit
@@ -37,7 +37,6 @@ RSpec.describe Users::PasswordsController, type: :controller do
     context "パスワード再設定に成功する場合" do
       it "ルート画面にリダイレクトする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        user.confirm
         raw = user.send_reset_password_instructions
         put :update, params: { user: { reset_password_token: raw, password: "12345678", password_confirmation: "12345678" } }
         user.reload
