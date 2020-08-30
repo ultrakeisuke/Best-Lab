9c92fb782c69018539cb5764e7540e38cfba35d1
ultrakeisuke <aucwm605@gmail.com>
2020-08-31 06:20:49 +0900

    Clean: ユーザーの認証に関する記述をリファクタリング。
    user.confirmとguest_user.confirmをbefore内に定義しました。

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Users::SessionsController, type: :controller do
   let(:user) { create(:user) }
   let(:guest_user) { create(:guest_user) }
+  before do
+    user.confirm
+    guest_user.confirm
+  end
 
   describe "newアクション" do
     it "正常なレスポンスとnewページを返す" do
@@ -16,7 +20,6 @@ RSpec.describe Users::SessionsController, type: :controller do
     context "ログインに失敗した場合" do
       it "取り扱い説明画面にリダイレクトする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        user.confirm
         post :create, params: { user: { email: "", password: "" } }
         expect(response).to have_http_status "302"
         expect(response).to redirect_to about_path
@@ -25,7 +28,6 @@ RSpec.describe Users::SessionsController, type: :controller do
     context "ログインに成功した場合" do
       it "ホーム画面にリダイレクトする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        user.confirm
         post :create, params: { user: { email: "user@example.com", password: "1234567" } }
         expect(response).to have_http_status "302"
         expect(assigns(:user)).to eq user
@@ -35,7 +37,6 @@ RSpec.describe Users::SessionsController, type: :controller do
     context "ゲストユーザーでログインした場合" do
       it "ホーム画面にリダイレクトする" do
         @request.env["devise.mapping"] = Devise.mappings[:user]
-        guest_user.confirm
         post :create, params: { user: { email: guest_user.email, password: "1234567" } }
         expect(response).to have_http_status "302"
         expect(assigns(:user)).to eq guest_user
@@ -46,7 +47,6 @@ RSpec.describe Users::SessionsController, type: :controller do
   describe "destroyアクション" do
     it "正常なレスポンスとページを返す" do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user.confirm
       delete :destroy, params: { id: user.id }
       expect(response).to have_http_status "302"
       expect(response).to redirect_to about_path
