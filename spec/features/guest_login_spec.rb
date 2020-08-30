44740bf3701fe35890d1ccc53146d64bcf914954
ultrakeisuke <aucwm605@gmail.com>
2020-08-30 19:35:49 +0900

    Add: ゲストユーザーに関するテストを追加。
    ゲストログインとアカウント削除の処理を確認しました。

---
+++ spec/features/guest_login_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature 'ゲストログインとアカウント削除' do
+  let(:guest_user) { create(:guest_user) }
+  background do
+    guest_user.confirm
+  end
+
+  scenario 'ゲストユーザーのアカウントを削除できない' do
+    visit about_path
+    click_link 'ゲストログイン(閲覧用)'
+    expect(page).to have_selector 'h1', text: 'ホーム'
+    click_link 'プロフィール'
+    click_link 'プロフィールを編集'
+    expect{ click_button 'アカウントの削除' }.not_to change(User, :count)
+    expect(page).to have_selector 'h3', text: guest_user.name
+  end
+
+end
+
