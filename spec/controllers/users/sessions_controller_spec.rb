d040ac44cb5a85ea5c777d557367eee5029a7757
ultrakeisuke <aucwm605@gmail.com>
2020-08-30 09:58:16 +0900

    Add: ゲストユーザーでログインする場合のテストを追加。

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Users::SessionsController, type: :controller do
   let(:user) { create(:user) }
+  let(:guest_user) { create(:guest_user) }
 
   describe "newアクション" do
     it "正常なレスポンスとnewページを返す" do
@@ -28,6 +29,16 @@ RSpec.describe Users::SessionsController, type: :controller do
         post :create, params: { user: { email: "user@example.com", password: "1234567" } }
         expect(response).to have_http_status "302"
         expect(assigns(:user)).to eq user
+        expect(response).to redirect_to root_path
+      end
+    end
+    context "ゲストユーザーでログインした場合" do
+      it "ホーム画面にリダイレクトする" do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        guest_user.confirm
+        post :create, params: { user: { email: guest_user.email, password: "1234567" } }
+        expect(response).to have_http_status "302"
+        expect(assigns(:user)).to eq guest_user
         expect(response).to redirect_to root_path
       end
     end
