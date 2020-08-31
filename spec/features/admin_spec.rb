3bb53b1861e4233aa4726ed354f28e95d622df74
ultrakeisuke <aucwm605@gmail.com>
2020-09-01 08:00:25 +0900

    Clean: ユーザー情報の閲覧に関するテストをリファクタリング。

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -40,17 +40,16 @@ RSpec.feature 'ユーザー情報の閲覧とアカウント削除' do
   
   scenario 'ユーザー情報の閲覧とアカウント削除が正常に作動する' do
     login_as_admin(admin)
-
     expect(page).to have_content 'すべてのユーザー'
     expect(all('li').size).to eq(25)
-    expect(page).to have_selector 'a', text: 'user-25'
-    expect(page).not_to have_selector 'a', text: 'user-26'
-
-    click_link 'user-25'
-    expect(page).to have_selector 'h3', text: 'user-25さんのプロフィール'
+    
+    click_link 'user-1'
+    expect(page).to have_selector 'h3', text: 'user-1さんのプロフィール'
     expect { click_button 'アカウントの削除' }.to change(User, :count).by(-1)
-    expect(page).to have_selector 'a', text: 'user-26'
 
+    click_link '最後'
+    expect(page).to have_current_path '/admins/users?page=4'
+    expect(all('li').size).to eq(24)
   end
 end
 
