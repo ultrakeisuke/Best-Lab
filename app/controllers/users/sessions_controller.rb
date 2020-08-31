b0b08672c44989333b24c16447a5589e722e7285
ultrakeisuke <aucwm605@gmail.com>
2020-09-01 02:10:52 +0900

    Add: ログイン後のリダイレクト先を新たに定義。
    ログイン後はホーム画面(users_basics_path)を表示するように定義しました。

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -32,6 +32,10 @@ class Users::SessionsController < Devise::SessionsController
       devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
     end
 
+    def after_sign_in_path_for(resource_name)
+      users_basics_path
+    end
+
     def after_sign_out_path_for(resource_name)
       root_path
     end
