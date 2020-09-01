d03e3a7e8df66e4a85e08084fd05eb2fd90d2462
ultrakeisuke <aucwm605@gmail.com>
2020-09-02 03:02:14 +0900

    Update: テストユーザーをimportメソッドで作成する方法にアップデート。
    gemのactiverecord-importを導入し、1回のクエリで複数のユーザーを作成できるように修正しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
+users = []
 100.times do |n|
   name = Faker::Name.name
   email = Faker::Internet.email
-  user = User.create(
-    name: name,
-    email: email,
-    password: "password",
-    password_confirmation: "password"
-  )
-  user.confirm
+  users << User.new(name: name,
+                    email: email,
+                    password: "1234567",
+                    password_confirmation: "1234567")
 end
+User.import users
 
 admin_user = Admin.create!(email: "admin@example.com",
                            password: "1234567")
