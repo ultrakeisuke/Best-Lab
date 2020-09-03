f6be49764f83fbda71322930c2cce30afaed4aee
ultrakeisuke <aucwm605@gmail.com>
2020-09-03 19:35:31 +0900

    Add: showメソッド内にダイレクトメッセージ用の処理を追加。

--- app/controllers/users/basics_controller.rb
+++ app/controllers/users/basics_controller.rb
@@ -9,6 +9,27 @@ class Users::BasicsController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # 参加するメッセージルーム情報を取得
+    @current_user_entry = Entry.where(user_id: current_user.id)
+    # 相手のメッセージルーム情報を取得
+    @another_user_entry = Entry.where(user_id: @user.id)
+
+    unless @current_user_entry.id == @another_user_entry.id
+      @current_user_entry.each do |current|
+        @another_user_entry.each do |another|
+          # ルームが存在する場合
+          if current.room_id == another.room_id
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      # メッセージルームが存在しない場合は新規作成
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
   
 end
