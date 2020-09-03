52b42639c298bd2382996dabb3fc209b6e0e6a9b
ultrakeisuke <aucwm605@gmail.com>
2020-09-03 20:08:21 +0900

    Add: Minimagicによる画質変更メソッドを追加。

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+module CarrierWave
+  module Minimagick
+    def quality(percentage)
+      manipulate! do |img|
+        img.quality(percentage.to_s)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
