d7dcdc71676f06c604230f9e06d104517065683d
ultrakeisuke <aucwm605@gmail.com>
2020-09-04 22:00:15 +0900

    Add: メッセージの作成、削除に関するアクションを追加。
    各処理に含まれるredirect_toの行き先は未定なので、今後設定します。

---
+++ app/controllers/users/messages_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Users::MessagesController < ApplicationController
+
+  def create
+    message = Message.new(message_params)
+    message.user_id = current_user_id
+    if massage.save
+      redirect_to room_path(message.room)
+    else
+      redirect_to 
+    end
+  end
+
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+    redirect_to 
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:room_id, :body)
+    end
+
+end
