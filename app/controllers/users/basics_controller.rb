90ea00fd349488336a2fc259c168c71e5367b613
ultrakeisuke <aucwm605@gmail.com>
2020-09-05 10:05:20 +0900

    Add: ダイレクトメッセージのための処理をshowアクションに追加。

--- app/controllers/users/basics_controller.rb
+++ app/controllers/users/basics_controller.rb
@@ -9,6 +9,27 @@ class Users::BasicsController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # ログインユーザーのすべてのentryを取得
+    @curernt_user_entry = Entry.where(user_id: current_user.id)
+    # メッセージ相手のすべてのentryを取得
+    @another_user_entry = Entry.where(user_id: @user.id)
+    # ログインユーザー以外のユーザーのプロフィール画面を表示した場合
+    unless current_user.id == @user.id
+      # ログインユーザーとメッセージ相手に共通のroomが存在するかどうかを
+      # entryから探し出し、存在の有無を@is_roomというインスタンスで識別する
+      @current_user_entry.each do |current|
+        @another_user_entry.each do |another|
+          if current.room_id == another.room_id
+            @is_room = true
+          end
+        end
+      end
+      # 共通のroomが存在しなかった場合、roomとentryのインスタンスを作成
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
   
 end
