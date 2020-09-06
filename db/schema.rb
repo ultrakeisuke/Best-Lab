2df8bb2ce692d60a6afa7df09c8e12217f786e4b
ultrakeisuke <aucwm605@gmail.com>
2020-09-07 07:16:22 +0900

    Add: messagesテーブルにstring型のpictureカラムを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_205859) do
+ActiveRecord::Schema.define(version: 2020_09_06_221411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_205859) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
