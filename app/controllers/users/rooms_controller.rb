9520ac200d256d6394d2fa517729eb1deadce75c
ultrakeisuke <aucwm605@gmail.com>
2020-09-06 11:07:19 +0900

    Fix: Entryが取得できないバグを修正しました。
    current_user_entry->current_entryに修正しました。同時にanother_user_entry->another_entryに統一しました。

--- app/controllers/users/rooms_controller.rb
+++ app/controllers/users/rooms_controller.rb
@@ -6,19 +6,19 @@ class Users::RoomsController < ApplicationController
     # メッセージルームを作成
     room = Room.create
     # Entryモデルにログインユーザーのレコードを作成
-    current_user_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
     # Entryモデルに相手のレコードを作成
-    another_user_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
     # 作成した部屋を表示
     redirect_to room_path(room)
   end
 
   def index
     # ログインユーザーのすべてのentryを取得
-    current_user_entries = current_user.entries
+    current_entries = current_user.entries
     my_room_ids = []
     # ログインユーザーのentryをもとに、room_idをひとつずつmy_room_idsに格納
-    current_user_entries.each do |entry|
+    current_entries.each do |entry|
       my_room_ids << entry.room.id
     end
     # ログインユーザーが参加しているすべてのroomから、user_idがログインユーザーでないレコードを取得
@@ -32,7 +32,7 @@ class Users::RoomsController < ApplicationController
     @message = Message.new
     # ひとつのroomが所属するentryは2つ(user_idがログインユーザー、相手)なので、
     # ログインユーザーでないほうのuser_idから取得できるentryはメッセージ相手のものとなる
-    @another_user_entry = @room.entries.find_by('user_id != ?', current_user.id)
+    @another_entry = @room.entries.find_by('user_id != ?', current_user.id)
   end
 
 end
