4e8d22ba86d07cd977356c112e4073cd79cd7cf5
ultrakeisuke <aucwm605@gmail.com>
2020-09-06 18:32:50 +0900

    Add: guest-loginブランチに作ってしまったDM関係のファイルをマージ。

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_09_02_205859) do
-=======
 ActiveRecord::Schema.define(version: 2020_09_04_234934) do
->>>>>>> guest-login
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
