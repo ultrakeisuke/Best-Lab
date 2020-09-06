1951499507aa4575193c58f2840bdbcadac74c1a
ultrakeisuke <aucwm605@gmail.com>
2020-09-06 11:04:35 +0900

    Fix: Entryが取得できないバグを修正しました。
    @current_user_entry->@current_entryに修正しました。

--- app/controllers/users/basics_controller.rb
+++ app/controllers/users/basics_controller.rb
@@ -10,15 +10,15 @@ class Users::BasicsController < ApplicationController
   def show
     @user = User.find(params[:id])
     # ログインユーザーのすべてのentryを取得
-    @curernt_user_entry = Entry.where(user_id: current_user.id)
+    @current_entry = Entry.where(user_id: current_user.id)
     # メッセージ相手のすべてのentryを取得
-    @another_user_entry = Entry.where(user_id: @user.id)
+    @another_entry = Entry.where(user_id: @user.id)
     # ログインユーザー以外のユーザーのプロフィール画面を表示した場合
     unless current_user.id == @user.id
       # ログインユーザーとメッセージ相手に共通のroomが存在するかどうかを
       # entryから探し出し、存在の有無を@is_roomというインスタンスで識別する
-      @current_user_entry.each do |current|
-        @another_user_entry.each do |another|
+      @current_entry.each do |current|
+        @another_entry.each do |another|
           if current.room_id == another.room_id
             @is_room = true
             @room_id = current.room_id
