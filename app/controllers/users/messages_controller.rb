3515135af8f4c3917063f3f504b49d2df394f702
ultrakeisuke <aucwm605@gmail.com>
2020-09-08 02:24:26 +0900

    Add: ストロングパラメータにuser_idを追加。

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -22,7 +22,7 @@ class Users::MessagesController < ApplicationController
   private
 
     def message_params
-      params.require(:message).permit(:room_id, :body, pictures_attributes: [:picture])
+      params.require(:message).permit(:user_id, :room_id, :body, pictures_attributes: [:picture])
     end
 
 end
