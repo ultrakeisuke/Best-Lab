bf0245801cc1dcc2fa46904a048679e238fb8b77
ultrakeisuke <aucwm605@gmail.com>
2020-09-10 01:38:29 +0900

    Add: 4枚の画像をメッセージ中のformにわたす設定を追加。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,3 +1,7 @@
 $(function () {
-  console.log("OK")
-});
+  let html;
+  for (let i = 1; i <= 3; i++) {
+    html = `<input type="file" name="message[pictures_attributes][${i}][picture]" id="message_pictures_attributes_${i}_picture">`
+    $(".message-pictures").append(html);
+  }
+})
