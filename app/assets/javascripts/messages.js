90b98be0ddb47c725e821276dc947b78a3d3d670
ultrakeisuke <aucwm605@gmail.com>
2020-09-11 08:01:16 +0900

    Change: 選択したファイルをプレビューで表示する仕様に変更。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,20 @@
 $(function () {
-  let html;
-  for (let i = 1; i <= 3; i++) {
-    html = `<input type="file" name="message[pictures_attributes][${i}][picture]" id="message_pictures_attributes_${i}_picture">`
-    $(".message-pictures").append(html);
-  }
-})
+  //querySelectorでfile_fieldを取得
+  var file_field = document.querySelector('input[type="file"]')
+  //fileが選択した際にイベントが発生
+  $('#picture-file').change(function () {
+    //アップロードするファイルにアクセス
+    var file = $('input[type="file"]').prop('files')[0];
+    //ファイルをURIとして読み込む処理を設定
+    var fileReader = new FileReader();
+    //読み込み完了後にファイルのURLをsrcに格納するイベントを設定
+    fileReader.onloadend = function () {
+      var src = fileReader.result
+      var html = `<img src="${src}" width="100" height="80">`
+      //#picture-containerの前にhtmlを挿入
+      $('#picture-container').before(html);
+    }
+    //読み込みを実行
+    fileReader.readAsDataURL(file);
+  });
+});
