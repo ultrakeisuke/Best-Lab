66a053df0ce5c1c2389f4f189c5b16037208afed
ultrakeisuke <aucwm605@gmail.com>
2020-09-13 11:00:01 +0900

    Add: ファイルのドラッグアンドドロップ用にDataTransferを追加。
    ファイルを追加するたびに#picture-containerのclassが変わり、それをもとにドロップボックスの幅を調整します。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,30 +1,46 @@
 $(function () {
+  //ドラッグされているデータを保持するためにDataTransferオブジェクトを作成
+  var dataBox = new DataTransfer();
   //querySelectorでfile_fieldを取得
   var file_field = document.querySelector('input[type="file"]')
   //fileが選択した際にイベントが発生
   $('#picture-file').change(function () {
     //アップロードするファイルにアクセス
-    var file = $('input[type="file"]').prop('files')[0];
-    //ファイルをURIとして読み込む処理を設定
-    var fileReader = new FileReader();
-    //読み込み完了後にファイルのURLをsrcに格納するイベントを設定
-    fileReader.onloadend = function () {
-      var src = fileReader.result
-      var html = `<div class='message-picture' data-picture="${file.name}">
-                    <div class='message-picture__content'>
-                      <div class='message-picture__content--icon'>
-                        <img src=${src} width='100' height='80'>
+    var files = $('input[type="file"]').prop('files')[0];
+    $.each(this.files, function (i, file) {
+      //ファイルをURIとして読み込む処理を設定
+      var fileReader = new FileReader();
+      //dataTransferにfileを追加
+      dataBox.items.add(file)
+      //転送中のファイル一覧をfile_fieldに代入
+      file_field.files = dataBox.files
+      //プレビューのファイル数をnumに代入
+      var num = $('.message-picture').length + 1 + i
+      //ブラウザに画像ファイルを表示
+      fileReader.readAsDataURL(file);
+      //画像が4枚になったらdataBoxを削除
+      if (num == 4) {
+        $('picture-container').css('display', 'none')
+      }
+      //読み込み完了後にファイルのURLをsrcに格納するイベントを設定
+      fileReader.onloadend = function () {
+        var src = fileReader.result
+        var html = `<div class='message-picture' data-picture="${file.name}">
+                      <div class='message-picture__content'>
+                        <div class='message-picture__content--icon'>
+                          <img src=${src} width='100' height='80'>
+                        </div>
                       </div>
-                    </div>
-                    <div class='message-picture__operation'>
-                      <div class='message-picture__operation--delete'>削除</div>
-                    </div>
-                  </div>`
-      //#picture-containerの前にhtmlを挿入
-      $('#picture-container').before(html);
-    }
-    //読み込みを実行
-    fileReader.readAsDataURL(file);
+                      <div class='message-picture__operation'>
+                        <div class='message-picture__operation--delete'>削除</div>
+                      </div>
+                    </div>`
+        //#picture-containerの前にhtmlを挿入
+        $('#picture-container').before(html);
+      }
+      //#picture-containerクラスを変更し、CSSでドロップボックスの大きさを変更
+      $('#picture-container').attr('class', `picture-num-${num}`)
+    });
   });
   $(document).on("click", '.message-picture__operation--delete', function () {
     //プレビュー要素(.message-picture)を取得
