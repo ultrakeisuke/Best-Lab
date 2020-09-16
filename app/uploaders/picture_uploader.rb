c1448389847e237de05811a6712d6e9ffe25aa5a
ultrakeisuke <aucwm605@gmail.com>
2020-09-17 02:35:26 +0900

    Change: デフォルト画像名をdefault.jpegに変更。

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -24,7 +24,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "/images/round-flask.jpeg"
+    "/images/default.jpeg"
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
