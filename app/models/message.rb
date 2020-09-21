6e3340524ce9e0f5632b66dd538e78422e177252
ultrakeisuke <aucwm605@gmail.com>
2020-09-21 11:22:24 +0900

    Add: メッセージの文字についてのバリデーションを追加。
    文字と画像が空の場合と、文字数が10000を超える場合にメッセージが送信できないようにしました。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  validates :body, length: { maximum: 10000 }, presence: true, unless: -> { pictures.present? }
   has_many :pictures, dependent: :destroy
   accepts_nested_attributes_for :pictures, allow_destroy: true
 end
