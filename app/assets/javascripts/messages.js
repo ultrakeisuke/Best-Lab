cbe4668fe76912bdaab0bac157c022ac3dc3726b
ultrakeisuke <aucwm605@gmail.com>
2020-09-29 06:48:26 +0900

    Add: 右クリックした場所にリストを表示する機能を追加。
    pageXとpageYで右クリックした要素の位置情報を取得しcssに反映させました。

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -134,9 +134,15 @@ $(function () {
 $(document).on("contextmenu", '.message-field__message', function (e) {
   //右クリックのデフォルトの表示をキャンセル
   e.preventDefault();
+  //右クリックした位置情報を取得
+  var posX = e.pageX;
+  var posY = e.pageY;
   //右クリックした要素のidと対応するリストのidを取得し表示
   id = $(this).attr('id') + "__menu"
-  document.getElementById(id).style.display = "block";
+  var contextmenu = document.getElementById(id)
+  contextmenu.style.display = "block";
+  contextmenu.style.left = posX + 'px';
+  contextmenu.style.top = posY + 'px';
   //リスト外をクリックしたときにリストを非表示にする処理
   $(document).on("click", function () {
     document.getElementById(id).style.display = "none";
