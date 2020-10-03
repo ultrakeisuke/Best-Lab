bba141e3efb6524957134123e6a095fa3018e44f
ultrakeisuke <aucwm605@gmail.com>
2020-10-03 23:23:17 +0900

    Change: roomとentryのデータを削除。

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,3 @@ User.import users
 
 admin_user = Admin.create!(email: "admin@example.com",
                            password: "1234567")
-
-rooms = []
-45.times do |n|
-  rooms << Room.new
-end
-Room.import rooms
-
-entries = []
-90.times do |n|
-  entries << Entry.new(user_id: 1..10,
-                       room_id: 1..45)
-end
-Entry.import entries
