fe327221440554213c78662130b70b841d1cd3d7
ultrakeisuke <aucwm605@gmail.com>
2020-10-10 13:07:57 +0900

    Add: 新しいメッセージルーム作成(createアクション)にentryのテストを追加。

--- spec/controllers/users/rooms_controller_spec.rb
+++ spec/controllers/users/rooms_controller_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Users::RoomsController, type: :controller do
   let(:user) { create(:user) }
+  let(:another_user) { create(:another_user) }
 
   describe "createアクション" do
     context "プロフィール画面でメッセージボタンをクリックした場合" do
-      it "新しいメッセージルームを作成" do
+      it "新しいメッセージルームとエントリーを作成" do
         login_user(user)
         # 1つのroomにつき、自分と相手2人分のentryが作成される
-        expect { post :create, params: { entry: { user_id: user.id } } }.to change(Entry, :count).by(2)
+        expect { post :create, params: { entry: { user_id: another_user.id } } }.to change(Entry, :count).by(2)
+        # 自分の相手のエントリーが作成される
+        expect(assigns(:current_entry)).to eq user.entries.last
+        expect(assigns(:another_entry)).to eq another_user.entries.last
         expect(response).to have_http_status "302"
         room = Room.last
         expect(response).to redirect_to users_room_path(room.id)
