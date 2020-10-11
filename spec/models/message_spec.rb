b161df3db8e430b4a887f9cb9ff031d2b52dbbf4
ultrakeisuke <aucwm605@gmail.com>
2020-10-11 17:06:35 +0900

    Add: メッセージの文字と画像が空なら無効になることを検証するテストを追加。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:room) { create(:room) }
+  let(:message) { create(:message, user_id: user.id, room_id: room.id)}
+  
+  it "メッセージの文字と画像が空なら無効" do
+    message.body = ""
+    message.pictures.blank?
+    expect(message).not_to be_valid
+  end
+
 end
