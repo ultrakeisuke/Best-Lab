b21812be5da93581f50940784d20d66b813906a4
ultrakeisuke <aucwm605@gmail.com>
2020-10-13 21:12:43 +0900

    Add: @current_entryの値を検証するテストを追加。

--- spec/controllers/users/basics_controller_spec.rb
+++ spec/controllers/users/basics_controller_spec.rb
@@ -26,12 +26,15 @@ RSpec.describe Users::BasicsController, type: :controller do
     it "インスタンスが期待した値を返し、正常なレスポンスを返す" do
       create_list(:rooms, 5)
       create_list(:current_entries, 5, user_id: user.id)
+      create_list(:another_entries, 1, user_id: another_user.id)
       current_entry = Entry.where(user_id: user.id)
+      another_entry = Entry.where(user_id: another_user.id)
       
       login_user(user)
       get :show, params: { id: another_user.id }
       expect(assigns(:user)).to eq another_user
       expect(assigns(:current_entry)).to eq current_entry
+      expect(assigns(:another_entry)).to eq another_entry
       expect(response).to have_http_status "200"
       expect(response).to render_template :show
     end
