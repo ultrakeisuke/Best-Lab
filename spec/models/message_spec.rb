a8fbdcd751e2a35f8fdd490c2f0687219b80d77a
ultrakeisuke <aucwm605@gmail.com>
2020-10-15 17:56:20 +0900

    Add: メッセージの画像が空で文字が空でない場合を検証するテストを追加。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,9 +6,14 @@ RSpec.describe Message, type: :model do
   let(:message) { create(:message, user_id: user.id, room_id: room.id)}
   
   it "メッセージの文字と画像が空なら無効" do
+    create_list(:picture, 4, message_id: message.id)
     message.body = ""
-    message.pictures.blank?
     expect(message).not_to be_valid
+  end
+
+  it "メッセージの画像が空で文字が空でないなら有効" do
+    message.body = "a"
+    expect(message).to be_valid
   end
 
 end
