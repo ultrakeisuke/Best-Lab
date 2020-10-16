4d4b5cb91251ab2776dcc695e4b49282fbc4c5fe
ultrakeisuke <aucwm605@gmail.com>
2020-10-16 20:07:42 +0900

    Add: 画像を6枚以上投稿できないバリデーションを追加。

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,14 @@
 class Picture < ApplicationRecord
+  MAX_PICTURES_COUNT = 5
+
   belongs_to :message
   mount_uploader :picture, PictureUploader
+  validate :pictures_count_must_be_within_limit
+
+  private
+  
+    def pictures_count_must_be_within_limit
+      errors.add(:base, "画像の最大投稿数は#{MAX_PICTURES_COUNT}枚です") if message.pictures.count >= MAX_PICTURES_COUNT
+    end
+
 end
