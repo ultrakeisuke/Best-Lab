4f543d35d21f4b66c5e7a33bb9aee5f11a001a30
ultrakeisuke <aucwm605@gmail.com>
2020-10-20 16:54:09 +0900

    Clean: メッセージに文章か画像が含まれればよい仕様をprivateメソッドでリファクタリング。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,15 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  validates :body, length: { maximum: 10000 }, presence: true, unless: -> { pictures.present? }
+  validates :body, length: { maximum: 10000 }
   has_many :pictures, dependent: :destroy
+  validates :body_or_pictures, presence: true
   accepts_nested_attributes_for :pictures, allow_destroy: true
+
+  private
+  # メッセージに文章か画像が含まれれば送信可能とする
+    def body_or_pictures
+      body.presence or pictures.presence
+    end
+
 end
