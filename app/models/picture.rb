3785d75bc4901a47269b3058ccade540da78f767
ultrakeisuke <aucwm605@gmail.com>
2020-10-20 18:15:44 +0900

    Add: 画像投稿数に関する説明をコメントで追加。

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,6 +6,7 @@ class Picture < ApplicationRecord
   validate :pictures_count_must_be_within_limit
 
   private
+    # メッセージに紐づく画像の枚数を制限する
     def pictures_count_must_be_within_limit
       if message && message.pictures.length > MAX_PICTURES_COUNT
         errors.add(:base, "投稿できる画像は#{MAX_PICTURES_COUNT}枚までです。")
