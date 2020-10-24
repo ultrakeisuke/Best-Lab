fbdd279793372a1933eb48735e99b0c2202531bb
ultrakeisuke <aucwm605@gmail.com>
2020-10-24 16:39:23 +0900

    Add: validation, mount_uploader, attribute情報を追加。

---
+++ app/forms/message_form.rb
@@ -0,0 +1,21 @@
+class MessageForm
+  # ActionPackやActionViewと連携。validationを使えるようにする
+  include ActiveModel::Model
+  # オブジェクト生成の際にパラメータに指定する属性(attribute)を定義できるようにする
+  include Virtus.model
+  # Model以外でCarrierWaveを使えるようにする
+  extend CarrierWave::Mount
+
+  validates :body, length: { maximum: 10000 }
+  validates :body_or_pictures, presence: true
+
+  attribute :user_id, Integer
+  attribute :room_id, Integer
+  attribute :body, String
+  attribute :picture, String
+
+  mount_uploader :picture, PictureUploader
+
+  private
+
+end
