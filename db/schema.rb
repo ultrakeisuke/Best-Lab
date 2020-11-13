fc782b0ef6f3a44c841301deaceeb35454152dbf
ultrakeisuke <aucwm605@gmail.com>
2020-11-13 22:01:33 +0900

    Add: profilesテーブルに外部キーのuser_idを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_121807) do
+ActiveRecord::Schema.define(version: 2020_11_13_125847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 2020_11_13_121807) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -106,4 +108,5 @@ ActiveRecord::Schema.define(version: 2020_11_13_121807) do
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
   add_foreign_key "pictures", "messages"
+  add_foreign_key "profiles", "users"
 end
