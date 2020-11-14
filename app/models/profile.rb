c449765c73eb1929390cd97d4b7253072a4598f1
ultrakeisuke <aucwm605@gmail.com>
2020-11-14 18:38:24 +0900

    Add: profileモデルを追加。
    userに紐付き、userとは1対1の関係です。

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,5 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+  # 所属を大学生、大学院生、高専生、専門学生、社会人、その他に分類
+  enum affiliation: %i[undergraduate graduate technical_college college working the_others]
 end
