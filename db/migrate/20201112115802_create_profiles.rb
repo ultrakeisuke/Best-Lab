6c37677d441b42fc1e42e71bf6a5d80834a189eb
ultrakeisuke <aucwm605@gmail.com>
2020-11-24 23:10:00 +0900

    Change: 削除したマイグレーションファイルの内容を移行して編集。

--- db/migrate/20201112115802_create_profiles.rb
+++ db/migrate/20201112115802_create_profiles.rb
@@ -1,12 +1,14 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-      t.integer :affiliation
-      t.string :university
+      t.string :affiliation, null: false, default: "大学生"
       t.string :school
+      t.string :faculty
       t.string :department
       t.string :laboratory
-      t.string :content
+      t.text :content
+
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_005836) do
+ActiveRecord::Schema.define(version: 2020_11_12_115802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,9 +65,9 @@ ActiveRecord::Schema.define(version: 2020_11_23_005836) do
     t.string "department"
     t.string "laboratory"
     t.text "content"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
