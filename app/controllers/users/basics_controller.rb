48416ed8bc10bd9bb52827e73646f9e033ffe1a0
ultrakeisuke <aucwm605@gmail.com>
2020-11-29 18:50:22 +0900

    Change: indexアクションを削除。
    ユーザーの詳細ページはログイン後にしか閲覧できないよう変更しました。

--- app/controllers/users/basics_controller.rb
+++ app/controllers/users/basics_controller.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class Users::BasicsController < ApplicationController
-  before_action :authenticate_user!, only: :index
-  
-  def index
-    @users = User.all
-  end
+  before_action :authenticate_user!, only: :show
   
   def show
     @user = User.find(params[:id])
