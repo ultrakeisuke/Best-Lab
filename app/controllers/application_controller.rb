c1322d762413c89915c004bed7d1538573a04f6e
ultrakeisuke <aucwm605@gmail.com>
2020-11-30 19:54:43 +0900

    Change: 検索フォーム表示用のメソッド名を変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :user_search
+  before_action :profile_search
 
   # 検索フォームをサイドバーに常に表示するためsearch_form_forの@qを定義
-  def user_search
+  def profile_search
     @q = Profile.ransack(params[:q])
   end
 
