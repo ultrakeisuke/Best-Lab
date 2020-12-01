8f365e852b0373e0e38acdb4631d9945fa20d38b
ultrakeisuke <aucwm605@gmail.com>
2020-12-02 07:02:39 +0900

    Change: contentカラムで複数のキーワードから検索できるよう変更。

--- app/controllers/searches/users_controller.rb
+++ app/controllers/searches/users_controller.rb
@@ -5,23 +5,41 @@ class Searches::UsersController < ApplicationController
 
   # ユーザーの検索一覧を表示
   def index
-    params[:q].each do |key, value|
-      params[:q].delete(key) if value.blank?
-    end
+    # URLで検索した場合は全件表示する
+    @profiles = Profile.all if params[:q].nil?
 
-    if params[:q].present?
-      @q = Profile.ransack(search_user_params)
+    # 検索ボタンを押して検索した場合
+    # 加えて、contentカラムを使って検索した場合
+    if !params[:q][:content_cont].empty?
+      split_keywords = params[:q][:content_cont].split(/[[:blank:]]+/) # キーワードを分割して格納する
+      if !split_keywords.empty? # キーワードが空でない場合は一つずつ検索する
+        split_keywords.each do |keyword|
+          @keyword_targets = Profile.where('content LIKE(?)', "%#{keyword}%")
+        end
+      else # キーワードに空白(スペース)のみ入力されていた場合は検索しない
+        @keyword_targets = []
+      end
+
+      # content以外のカラムに値が入っていれば、そこからプロフィールを検索する
+      # params[:q][:content]を無視して検索したいので空にする
+      params[:q][:content_cont] = ""
+
+      # content以外のカラムで得た結果を@general_targetsと定義する
+      @general_targets = Profile.ransack(params[:q]).result
+
+      # contentとcontent以外で得たプロフィールから共通するデータを選出する
+      @keyword_targets.each do |k_target|
+        @general_targets.each do |g_target|
+          if k_target == g_target
+            @profiles = Profile.where(id: k_target)
+          end
+        end
+      end
+    else 
+      # contentカラムを使わず検索した場合
+      @q = Profile.ransack(params[:q])
       @profiles = @q.result.page(params[:page])
-      flash.now[:notice] = "お探しのユーザーは見つかりませんでした。" if @profiles.blank?
-    else
-      flash.now[:notice] = "最低でも1つの項目を入力してください。"
     end
   end
-
-  private
-
-    def search_user_params
-      params.require(:q).permit(:affiliation_eq, :school_eq, :faculty_eq, :department_eq, :laboratory_eq, :content_cont)
-    end
 
 end
