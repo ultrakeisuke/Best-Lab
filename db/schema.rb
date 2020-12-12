1d55a23ce8f80fe4f41af07cd679fcbef9fa50c3
ultrakeisuke <aucwm605@gmail.com>
2020-12-12 16:30:52 +0900

    Add: categories, posts, repliesテーブルを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_115802) do
+ActiveRecord::Schema.define(version: 2020_12_12_072310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2020_11_12_115802) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -58,6 +66,18 @@ ActiveRecord::Schema.define(version: 2020_11_12_115802) do
     t.index ["message_id"], name: "index_pictures_on_message_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.boolean "status", default: true, null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string "affiliation", default: "大学生", null: false
     t.string "school"
@@ -69,6 +89,16 @@ ActiveRecord::Schema.define(version: 2020_11_12_115802) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string "boty"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_replies_on_post_id"
+    t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -107,5 +137,8 @@ ActiveRecord::Schema.define(version: 2020_11_12_115802) do
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
   add_foreign_key "pictures", "messages"
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "replies", "users"
 end
