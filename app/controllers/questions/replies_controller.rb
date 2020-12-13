7744c5be7a3a181494ecb33433729d1c3420cd11
ultrakeisuke <aucwm605@gmail.com>
2020-12-13 23:49:27 +0900

    Add: repliesコントローラを追加。

---
+++ app/controllers/questions/replies_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Questions::RepliesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+end
