f06b3183dc78d0dd04717e41e4199000f47e57de
ultrakeisuke <aucwm605@gmail.com>
2020-12-20 11:32:00 +0900

    Change: statusをbooleanからstringに変更。

--- db/migrate/20201209120026_create_posts.rb
+++ db/migrate/20201209120026_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :title, null: false
       t.text :content, null: false
-      t.boolean :status, default: "open", null: false
+      t.string :status, default: "受付中", null: false
 
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
