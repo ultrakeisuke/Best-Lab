b450a0ba6d44924eb90cec72ade6506aa123d527
ultrakeisuke <aucwm605@gmail.com>
2020-12-21 20:37:25 +0900

    Change: 画像情報をポリモーフィック関連付けする仕様に変更。

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,17 +1,4 @@
 class Picture < ApplicationRecord
-  MAX_PICTURES_COUNT = 4
-
-  belongs_to :message, optional: true
-  belongs_to :post, optional: true
+  belongs_to :imageable, polymorphic: true
   mount_uploader :picture, PictureUploader
-  validate :pictures_count_must_be_within_limit
-
-  private
-    # メッセージに紐づく画像の枚数を制限する
-    def pictures_count_must_be_within_limit
-      if message && message.pictures.length > MAX_PICTURES_COUNT
-        errors.add(:base, "投稿できる画像は#{MAX_PICTURES_COUNT}枚までです。")
-      end
-    end
-
 end
