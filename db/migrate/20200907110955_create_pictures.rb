5c22206299fca722cbb4ef59c6a1ea87984fd425
ultrakeisuke <aucwm605@gmail.com>
2020-12-23 05:20:55 +0900

    Fix: null: falseをimageableにつけて修正。

--- db/migrate/20200907110955_create_pictures.rb
+++ db/migrate/20200907110955_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration[6.0]
   def change
     create_table :pictures do |t|
       t.string :picture
-      t.references :imageable, polymorphic: true
+      t.references :imageable, polymorphic: true, null: false
       t.timestamps
     end
   end
