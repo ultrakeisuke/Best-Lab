08836d4f75a0d497ab1610734e84e35fb6e0e002
ultrakeisuke <aucwm605@gmail.com>
2020-12-27 18:37:20 +0900

    Fix: typoを修正。

--- db/migrate/20201209120526_create_replies.rb
+++ db/migrate/20201209120526_create_replies.rb
@@ -1,11 +1,9 @@
 class CreateReplies < ActiveRecord::Migration[6.0]
   def change
     create_table :replies do |t|
-      t.string :boty
-
+      t.string :body
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, fereign_key: true
-
+      t.references :post, null: false, foreign_key: true
       t.timestamps
     end
   end
