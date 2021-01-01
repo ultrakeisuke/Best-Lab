d048c58fa904bdc1c5e6e2450bcc0d9e82501a91
ultrakeisuke <aucwm605@gmail.com>
2021-01-01 13:52:25 +0900

    Fix: リプライのエラーが表示されない、投稿内容が反映されない不具合を修正。

--- app/controllers/questions/replies_controller.rb
+++ app/controllers/questions/replies_controller.rb
@@ -10,6 +10,7 @@ class Questions::RepliesController < ApplicationController
     respond_to do |format|
       if @reply.save
         @answer = Answer.find(@reply.answer_id)
+        @answers = Answer.where(post_id: @reply.post_id)
         @post = Post.find(@reply.post_id)
         format.html { redirect_to questions_post_path(@reply.post_id) }
         format.js
@@ -20,10 +21,32 @@ class Questions::RepliesController < ApplicationController
     end
   end
 
+  # リプライの編集
+  def update
+    # パラメータ中のidをもとに編集するリプライを選出する
+    @target_reply = Reply.find(reply_params[:id])
+    @reply = ReplyForm.new(@target_reply)
+    # パラメータからidを削除してリプライを更新する
+    params = reply_params
+    params.delete(:id)
+    @reply.assign_attributes(params)
+    respond_to do |format|
+      if @reply.save
+        @answer = Answer.find(@reply.answer_id)
+        @post = Post.find(@reply.post_id)
+        format.html { redirect_to questions_post_path(@reply.post_id) }
+        format.js
+      else
+        format.html { render "questions/posts/show" }
+        format.js { render :update_errors }
+      end
+    end
+  end
+
   private
 
     def reply_params
-      params.require(:reply_form).permit(:body, :post_id, :answer_id, pictures_attributes: [:picture]).merge(user_id: current_user.id)
+      params.require(:reply_form).permit(:id, :body, :post_id, :answer_id, pictures_attributes: [:picture]).merge(user_id: current_user.id)
     end
 
 end
