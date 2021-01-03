539b9530bf7657eb5e9c2d2c45b355a8c03c01b7
ultrakeisuke <aucwm605@gmail.com>
2021-01-03 11:24:17 +0900

    Add: 回答に関するテストを追加。

---
+++ spec/controllers/questions/answers_controller_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+
+RSpec.describe Questions::AnswersController, type: :controller do
+  let(:user) { create(:user) }
+  let!(:parent_category) { create(:parent_category) }
+  let!(:children_category) { create(:children_category, ancestry: parent_category.id) }
+  let!(:test_post) { create(:post, user_id: user.id, category_id: children_category.id) }
+
+  # createアクションに関するテスト
+  describe '非同期通信によるcreateアクション' do
+    context "フォーム入力が無効であった場合" do
+      it "回答の作成に失敗する" do
+        login_user(user)
+        expect do
+        post :create, xhr: true, params: { answer_form: { post_id: test_post.id, body: "" } }
+        end.not_to change(Answer, :count)
+        expect(response).to have_http_status "200"
+        expect(response).to render_template "questions/answers/errors"
+      end
+    end
+    context "フォーム入力が有効であった場合" do
+      let(:answer_form) { build(:answer_form) }
+      it "回答の作成に成功する" do
+        login_user(user)
+        expect do
+        post :create, xhr: true, params: { answer_form: { post_id: test_post.id, body: answer_form.body } }
+        end.to change(Answer, :count).by(1)
+        expect(response).to have_http_status "200"
+        expect(response).to render_template "questions/answers/create"
+      end
+    end
+  end
+
+  describe '同期通信によるcreateアクション' do
+    context "フォーム入力が無効であった場合" do
+      it "回答の作成に失敗する" do
+        login_user(user)
+        expect do
+        post :create, params: { answer_form: { post_id: test_post.id, body: "" } }
+        end.not_to change(Answer, :count)
+        expect(response).to have_http_status "200"
+        expect(response).to render_template "questions/posts/show"
+      end
+    end
+    context "フォーム入力が有効であった場合" do
+      let(:answer_form) { build(:answer_form, post_id: test_post.id) }
+      it "回答の作成に成功する" do
+        login_user(user)
+        expect do
+        post :create, params: { answer_form: { post_id: test_post.id, body: answer_form.body } }
+        end.to change(Answer, :count).by(1)
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to questions_post_path(answer_form.post_id)
+      end
+    end
+  end
+
+  # updateアクションに関するテスト
+  describe '非同期通信によるupdateアクション' do
+    let!(:answer) { create(:answer, user_id: user.id, post_id: test_post.id) }
+    context "フォーム入力が無効であった場合" do
+      it "回答の編集に失敗する" do
+        login_user(user)
+        patch :update, xhr: true, params: { id: answer.id, answer_form: { id: answer.id,
+                                                                          post_id: test_post.id,
+                                                                          body: "" } }
+        expect(answer.reload.body).not_to eq ""
+        expect(answer.reload.body).to eq "answer"
+        expect(response).to have_http_status "200"
+        expect(response).to render_template "questions/answers/errors"
+      end
+    end
+    context "フォーム入力が有効であった場合" do
+      it "回答の編集に成功する" do
+        login_user(user)
+        patch :update, xhr: true, params: { id: answer.id, answer_form: { id: answer.id,
+                                                                          post_id: test_post.id,
+                                                                          body: "edited_answer" } }
+        expect(answer.reload.body).to eq "edited_answer"
+        expect(response).to have_http_status "200"
+        expect(response).to render_template "questions/answers/update"
+      end
+    end
+  end
+
+  describe '同期通信によるupdateアクション' do
+    let!(:answer) { create(:answer, user_id: user.id, post_id: test_post.id) }
+    context "フォーム入力が無効であった場合" do
+      it "回答の編集に失敗する" do
+        login_user(user)
+        patch :update, params: { id: answer.id, answer_form: { id: answer.id,
+                                                               post_id: test_post.id,
+                                                               body: "" } }
+        expect(answer.reload.body).not_to eq ""
+        expect(answer.reload.body).to eq "answer"
+        expect(response).to have_http_status "200"
+        expect(response).to render_template "questions/posts/show"
+      end
+    end
+    context "フォーム入力が有効であった場合" do
+      it "回答の編集に成功する" do
+        login_user(user)
+        patch :update, params: { id: answer.id, answer_form: { id: answer.id,
+                                                               post_id: test_post.id,
+                                                               body: "edited_answer" } }
+        expect(answer.reload.body).to eq "edited_answer"
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to questions_post_path(answer.post_id)
+      end
+    end
+  end
+
+end
