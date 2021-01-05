944b50ce3cd941a242ba0d56f513216652bd0138
ultrakeisuke <aucwm605@gmail.com>
2021-01-05 22:07:39 +0900

    Add: 検索フォームを常に表示する設定を追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :profile_search
+  before_action :post_search
 
-  # 検索フォームをサイドバーに常に表示するためsearch_form_forの@qを定義
+  # ユーザーの検索フォームをサイドバーに常に表示する処理
   def profile_search
     @q = Profile.ransack(params[:q])
+  end
+
+  # 質問の検索フォームをサイドバーに常に表示する処理
+  def post_search
+    @post_search = Post.ransack(params[:q])
+    # カテゴリーのセレクトボックスに初期値をセット
+    @parent_categories_for_search = Category.where(ancestry: nil)
+    @children_categories_for_search = @parent_categories_for_search.first.children
   end
 
   protected
