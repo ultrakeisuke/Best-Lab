54f1a932ad18046d9accf3c09275fa5b2ba60b9a
ultrakeisuke <aucwm605@gmail.com>
2021-01-14 06:53:26 +0900

    Change: コメントに関する記述を一部jqueryからjsに変更。
    この変更はまだ完了していません。

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,49 +1,54 @@
 // 回答に関する処理
-$(function () {
+function clickAnswerContents() {
   // 回答編集用のアイコンをクリックしたときの処理
   $(document).on('click', '.edit-answer-icon', function () {
     // 回答を非表示にする処理
-    var answerID = $(this).data('answer-id');
-    $(`#answer-contents-${answerID}`).addClass("hide");
+    var answerID = this.getAttribute('data-answer-id');
+    document.getElementById('answer-contents-' + answerID).classList.add('hide');
 
     // 回答の編集フォームを表示する処理
-    $(`#edit-answer-form-${answerID}`).removeClass("hide");
-    $(`#edit-answer-form-${answerID}`).addClass("show");
+    document.getElementById('edit-answer-form-' + answerID).classList.remove('hide');
+    document.getElementById('edit-answer-form-' + answerID).classList.add('show');
   });
 
-  // 回答の編集をキャンセルするときの処理
+  // 回答編集フォームのキャンセルボタンをクリックした時の処理
   $(document).on('click', '.edit-answer-cancel', function () {
     // 編集フォームを非表示にする処理
-    var answerID = $(this).data('answer-cancel');
-    $(`#edit-answer-form-${answerID}`).removeClass("show");
-    $(`#edit-answer-form-${answerID}`).addClass("hide");
+    var answerID = this.getAttribute('data-answer-cancel');
+    document.getElementById('edit-answer-form-' + answerID).classList.remove('show');
+    document.getElementById('edit-answer-form-' + answerID).classList.add('hide');
 
     // 回答を表示する処理
-    $(`#answer-contents-${answerID}`).removeClass("hide");
+    document.getElementById('answer-contents-' + answerID).classList.remove('hide');
   });
-});
+}
+
+clickAnswerContents();
+
 
 // リプライに関する処理
-$(function () {
+function clickReplyContents() {
   // リプライ編集用のアイコンをクリックしたときの処理
   $(document).on('click', '.edit-reply-icon', function () {
     // リプライを非表示にする処理
-    var replyID = $(this).data('reply-id');
-    $(`#reply-contents-${replyID}`).addClass("hide");
+    var replyID = this.getAttribute('data-reply-id');
+    document.getElementById('reply-contents-' + replyID).classList.add('hide');
 
     // リプライの編集フォームを表示する処理
-    $(`#edit-reply-form-${replyID}`).removeClass("hide");
-    $(`#edit-reply-form-${replyID}`).addClass("show");
+    document.getElementById('edit-reply-form-' + replyID).classList.remove('hide');
+    document.getElementById('edit-reply-form-' + replyID).classList.add('show');
   });
 
   // リプライ編集をキャンセルするときの処理
   $(document).on('click', '.edit-reply-cancel', function () {
     // 編集フォームを非表示にする処理
-    var replyID = $(this).data('reply-cancel');
-    $(`#edit-reply-form-${replyID}`).removeClass("show");
-    $(`#edit-reply-form-${replyID}`).addClass("hide");
+    var replyID = this.getAttribute('data-reply-cancel');
+    document.getElementById('edit-reply-form-' + replyID).classList.remove('show');
+    document.getElementById('edit-reply-form-' + replyID).classList.add('hide');
 
     // リプライを表示する処理
-    $(`#reply-contents-${replyID}`).removeClass("hide");
+    document.getElementById('reply-contents-' + replyID).classList.remove('hide');
   });
-});
+}
+
+clickReplyContents();
