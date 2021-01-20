28e364f02d97e569a5d764dc168cdda08d429751
ultrakeisuke <aucwm605@gmail.com>
2021-01-21 07:29:48 +0900

    Clean: メッセージ作成時刻をもとにソートする処理を編集。

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,10 @@ class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :room
   include Partner
+
+  # メッセージが１つでも存在する部屋のメッセージを返す処理
+  def include_message
+    self.room.messages.last if self.room.messages.present?
+  end
+
 end
