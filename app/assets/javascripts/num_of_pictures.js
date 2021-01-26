5d626ebafc69b5bf55784c58622c8a86dc3413c2
ultrakeisuke <aucwm605@gmail.com>
2021-01-26 21:08:34 +0900

    Revert "Revert "Merge branch 'fix-direct-messages'""
    This reverts commit 4043c6dcc28615f0493b4082823fb8491a828e1a.

---
+++ app/assets/javascripts/num_of_pictures.js
@@ -0,0 +1,21 @@
+// 画像選択に関する処理
+$(function () {
+  // 選択した画像の枚数をカメラアイコンの代わりに表示する処理
+  $('#picture-file').change(function () {
+    var files = this.files;
+    $('#picture-file-icon').replaceWith(`<i id="num-of-files">${files.length}</i>`);
+    if (files.length > 4) {
+      $('#num-of-files').addClass('too-many-files');
+    }
+    // 画像の再選択に関する処理
+    if ($('#num-of-files')) {
+      $('#picture-file').change(function () {
+        var files = this.files;
+        $('#num-of-files').replaceWith(`<i id="num-of-files">${files.length}</i>`);
+        if (files.length > 4) {
+          $('#num-of-files').addClass('too-many-files');
+        }
+      });
+    }
+  });
+});
