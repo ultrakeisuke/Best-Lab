69a4444ed4fd03f97c6cfb74ee56205dfd7ccb1e
ultrakeisuke <aucwm605@gmail.com>
2021-01-28 20:34:33 +0900

    Clean: ユーザーのプロフィール表示部を改修。
    所属によってテキストの背景色が変化する処理を修正しました。

--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -1,9 +1,19 @@
-//所属ごとにテキストの背景色が変わる処理
-window.onload = function () {
-  var affiliation = document.getElementById('user_affiliation').textContent;
-  if (affiliation == "大学生") {
-    $('#user_affiliation').attr('class', 'undergraduate');
-  } else {
-    $('#user_affiliation').attr('class', 'graduate');
+// 所属ごとにテキストの背景色が変わる処理
+window.addEventListener('load', function () {
+  var affiliation = document.getElementById('user_affiliation');
+  if (affiliation) {
+    if (affiliation.textContent == "大学生") {
+      document.getElementById('user_affiliation').classList.add('undergraduate');
+    } else if (affiliation.textContent == "大学院生") {
+      document.getElementById('user_affiliation').classList.add('graduate');
+    } else if (affiliation.textContent == "高専生") {
+      document.getElementById('user_affiliation').classList.add('technical-students');
+    } else if (affiliation.textContent == "専門学生") {
+      document.getElementById('user_affiliation').classList.add('professional-students');
+    } else if (affiliation.textContent == "社会人") {
+      document.getElementById('user_affiliation').classList.add('working');
+    } else if (affiliation.textContent == "その他") {
+      document.getElementById('user_affiliation').classList.add('other');
+    }
   }
-}
+});
