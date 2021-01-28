95ba366d6f330e2f91e27850b461f733a450ffbf
ultrakeisuke <aucwm605@gmail.com>
2021-01-28 20:53:44 +0900

    Add: すべてのプロフィール項目をユーザー詳細画面に追加。

--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -1,19 +1,19 @@
 // 所属ごとにテキストの背景色が変わる処理
 window.addEventListener('load', function () {
-  var affiliation = document.getElementById('user_affiliation');
+  var affiliation = document.getElementById('user-affiliation');
   if (affiliation) {
     if (affiliation.textContent == "大学生") {
-      document.getElementById('user_affiliation').classList.add('undergraduate');
+      document.getElementById('user-affiliation').classList.add('undergraduate');
     } else if (affiliation.textContent == "大学院生") {
-      document.getElementById('user_affiliation').classList.add('graduate');
+      document.getElementById('user-affiliation').classList.add('graduate');
     } else if (affiliation.textContent == "高専生") {
-      document.getElementById('user_affiliation').classList.add('technical-students');
+      document.getElementById('user-affiliation').classList.add('technical-students');
     } else if (affiliation.textContent == "専門学生") {
-      document.getElementById('user_affiliation').classList.add('professional-students');
+      document.getElementById('user-affiliation').classList.add('professional-students');
     } else if (affiliation.textContent == "社会人") {
-      document.getElementById('user_affiliation').classList.add('working');
+      document.getElementById('user-affiliation').classList.add('working');
     } else if (affiliation.textContent == "その他") {
-      document.getElementById('user_affiliation').classList.add('other');
+      document.getElementById('user-affiliation').classList.add('other');
     }
   }
 });
