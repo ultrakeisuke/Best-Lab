af1752c857a7a0bdff512b08537f3cd91b7abd00
ultrakeisuke <aucwm605@gmail.com>
2021-01-30 10:32:47 +0900

    Fix: インスタンス変数に関するテストを修正。

--- spec/controllers/users/basics_controller_spec.rb
+++ spec/controllers/users/basics_controller_spec.rb
@@ -2,41 +2,44 @@ require 'rails_helper'
 
 RSpec.describe Users::BasicsController, type: :controller do
   let(:user) { create(:user) }
-
-  describe 'indexアクション' do
-    context "ログインしていない場合" do
-      it "ログイン画面にリダイレクトする" do
-        get :index
-        expect(response).to have_http_status "302"
-        expect(response).to redirect_to new_user_session_path
-      end
-    end
-    context "ログインしている場合" do
-      it "indexページを返す" do
-        login_user(user)
-        get :index
-        expect(response).to have_http_status "200"
-        expect(response).to render_template :index
-      end
-    end
-  end
+  let(:another_user) { create(:another_user) }
+  let!(:parent_category) { create(:parent_category) }
+  let!(:children_category) { create(:children_category, ancestry: parent_category.id) }
+  let(:posts) { create_list(:post, 2, user_id: another_user.id, category_id: children_category.id) }
 
   describe 'showアクション' do
-    let(:another_user) { create(:another_user) }
-    it "インスタンスが期待した値を返し、正常なレスポンスを返す" do
-      create_list(:rooms, 5)
-      create_list(:current_entries, 5, user_id: user.id)
-      create_list(:another_entries, 1, user_id: another_user.id)
-      current_entry = Entry.where(user_id: user.id)
-      another_entry = Entry.where(user_id: another_user.id)
-      
-      login_user(user)
-      get :show, params: { id: another_user.id }
-      expect(assigns(:user)).to eq another_user
-      expect(assigns(:current_entry)).to eq current_entry
-      expect(assigns(:another_entry)).to eq another_entry
-      expect(response).to have_http_status "200"
-      expect(response).to render_template :show
+    context "相手が共通のメッセージルームを持たないユーザーの場合" do
+      before do
+        room1 = create(:room)
+        room2 = create(:room)
+        create(:entry, user_id: user.id, room_id: room1.id)
+        create(:entry, user_id: another_user.id, room_id: room2.id)
+      end
+      it "インスタンスが期待した値を返し、正常なレスポンスを返す" do
+        login_user(user)
+        get :show, params: { id: another_user.id }
+        expect(assigns(:posts)).to eq posts
+        # 共通のメッセージルームがないので@is_roomがnilを返す
+        expect(assigns(:is_room)).to eq nil
+        expect(response).to have_http_status "200"
+        expect(response).to render_template :show
+      end
+    end
+    context "相手が共通のメッセージルームを持つユーザーの場合" do
+      before do
+        room = create(:room)
+        create(:entry, user_id: user.id, room_id: room.id)
+        create(:entry, user_id: another_user.id, room_id: room.id)
+      end
+      it "インスタンスが期待した値を返し、正常なレスポンスを返す" do
+        login_user(user)
+        get :show, params: { id: another_user.id }
+        expect(assigns(:posts)).to eq posts
+        # 共通のメッセージルームがあるので@is_roomがtrueを返す
+        expect(assigns(:is_room)).to eq true
+        expect(response).to have_http_status "200"
+        expect(response).to render_template :show
+      end
     end
   end
 
