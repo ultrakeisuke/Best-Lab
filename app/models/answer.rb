1eb471f46bacf23f696e58464f07e30293764392
ultrakeisuke <aucwm605@gmail.com>
2021-01-31 17:46:52 +0900

    Change: postとanswerが削除された場合に子モデルも削除するよう変更。

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   has_many :pictures, as: :imageable
-  has_many :replies
+  has_many :replies, dependent: :destroy
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :pictures, as: :imageable
-  has_many :answers
-  has_many :replies
+  has_many :answers, dependent: :destroy
+  has_many :replies, dependent: :destroy
   belongs_to :user
   belongs_to :category
 end
