3bbe4232687221e5f61bd0238a5e3842156d683a
ultrakeisuke <aucwm605@gmail.com>
2021-02-02 07:44:00 +0900

    Add: 英語で保存されたaffiliationの値を日本語で表示するメソッドを追加。

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,18 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  AFFILIATIONS = { undergraduate: '大学生',
+                   graduate: '大学院生',
+                   technical_students: '高専生',
+                   professional_students: '専門学生',
+                   working: '社会人',
+                   other: 'その他' }
+
+  # 英語名で保存された所属(affiliation)を日本語で表示
+  def translated_affiliation
+    AFFILIATIONS.each do |key, value|
+      return value if self.affiliation == key.to_s
+    end
+  end
+  
 end
