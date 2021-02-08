6aa91c457137777acd0e1d6dbd1b69584af5706e
ultrakeisuke <aucwm605@gmail.com>
2021-02-08 21:47:56 +0900

    Add: 質問の投稿状態を日本語表記に変換するメソッドを追加。

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  STATUS = { open: "受付中", closed: "解決済" }
+
   # 自己解決した場合の処理
   def solved_by_questioner
     best_answer = Answer.find_by(post_id: self, user_id: self.user_id)
@@ -12,4 +14,10 @@ class Post < ApplicationRecord
     self.best_answer_id = best_answer.id
     self.save
   end
+
+  # 英語名で保存された投稿状態(status)を日本語で表示 
+  def translated_status
+    STATUS[self.status.to_sym]
+  end
+
 end
