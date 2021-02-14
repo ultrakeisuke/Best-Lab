e4a0803b62f9d899dd869444bdcb4ac920092f6c
ultrakeisuke <aucwm605@gmail.com>
2021-02-14 22:21:38 +0900

    Clean: admin用テストデータを改修。

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,21 +2,7 @@ FactoryBot.define do
 
   factory :admin do
     email { 'admin@example.com' }
-    password { '1234567' }
+    password { 'password' }
   end
 
-  sequence :users_name do |i|
-    "user-#{i}"
-  end
-
-  sequence :users_email do |i|
-    "user-#{i}@example.com"
-  end
-
-  factory :users, class: User do
-    name { generate :users_name }
-    email { generate :users_email }
-    password { '1234567' }
-  end
-  
 end
