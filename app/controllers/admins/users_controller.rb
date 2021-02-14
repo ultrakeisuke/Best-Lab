d61bbf9b3fc79d9f4955a9c9519ecd64f1fa8fed
ultrakeisuke <aucwm605@gmail.com>
2021-02-14 22:09:59 +0900

    Update: admins/users_controllerを機能修正。

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,18 +2,30 @@
 
 class Admins::UsersController < ApplicationController
   before_action :authenticate_admin!
+  before_action :check_user, only: [:show, :destroy]
+
+  PER = 10
 
   def index
-    @users = User.all.page(params[:page])
+    @users = User.ransack(params[:q]).result.page(params[:page]).per(PER)
   end
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user).page(params[:page]).per(PER)
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    redirect_to admins_users_path
+    user = User.find(params[:id])
+    user.inactivate_account
+    redirect_to admins_user_path(user)
   end
+
+  private
+
+    # DBに存在しないユーザーidを入力するとrootにリダイレクト
+    def check_user
+      redirect_to root_path unless User.exists?(id: params[:id])
+    end
   
 end
--- spec/controllers/admins/users_controller_spec.rb
+++ spec/controllers/admins/users_controller_spec.rb
@@ -1,59 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Admins::UsersController, type: :controller do
-  let(:user) { create(:user) }
-  let(:admin) { create(:admin) }
+  let!(:user) { create(:user) }
+  let!(:admin) { create(:admin) }
+  let!(:parent_category) { create(:parent_category) }
+  let!(:children_category) { create(:children_category, ancestry: parent_category.id) }
 
-  describe "indexアクション" do
-    context "ログインしていない場合" do
-      it "ホーム画面にリダイレクト" do
-        get :index
-        expect(response).to have_http_status "302"
-        expect(response).to redirect_to new_admin_session_path
-      end
-    end
-    context "ログインしている場合" do
-      it "indexページを返す" do
-        sign_in admin
-        get :index
-        expect(response).to have_http_status "200"
-        expect(response).to render_template :index
-      end
-    end
-  end
   describe "showアクション" do
-    context "ログインしていない場合" do
-      it "ホーム画面にリダイレクト" do
-        get :show, params: { id: user.id }
+    context "DBに存在しないユーザーのidを入力した場合" do
+      it "rootにリダイレクトする" do
+        login_admin(admin)
+        get :show, params: { id: 0 }
         expect(response).to have_http_status "302"
-        expect(response).to redirect_to new_admin_session_path
+        expect(response).to redirect_to root_path
       end
     end
-    context "ログインしている場合" do
-      it "showページを返す" do
-        sign_in admin
-        get :show, params: { id: user.id }
+    context "DBに存在するユーザーのidを入力した場合" do
+      it "ユーザー詳細画面を表示する" do
+        login_admin(admin)
+        get :show, params: { id: user }
         expect(response).to have_http_status "200"
         expect(response).to render_template :show
       end
     end
   end
+
   describe "destroyアクション" do
-    context "ログインしていない場合" do
-      it "ホーム画面にリダイレクト" do
-        get :destroy, params: { id: user.id }
+    context "DBに存在しないユーザーを削除しようとした場合" do
+      it "削除に失敗し、rootにリダイレクトする" do
+        login_admin(admin)
+        delete :destroy, params: { id: 0 }
         expect(response).to have_http_status "302"
-        expect(response).to redirect_to new_admin_session_path
+        expect(response).to redirect_to root_path
       end
     end
-    context "ログインしている場合" do
-      it "aboutページを返す" do
-        sign_in admin
-        get :destroy, params: { id: user.id }
+    context "DBに存在するユーザーを削除しようとした場合" do
+      it "削除に成功し、ユーザー詳細画面にリダイレクトする" do
+        login_admin(admin)
+        delete :destroy, params: { id: user }
         expect(response).to have_http_status "302"
-        expect(response).to redirect_to admins_users_path
+        expect(response).to redirect_to admins_user_path(user)
       end
     end
   end
-
 end
