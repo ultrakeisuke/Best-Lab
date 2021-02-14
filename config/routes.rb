a9a2b6ceb8b70551b6e6541b47ed8702a30b1494
ultrakeisuke <aucwm605@gmail.com>
2021-02-14 22:29:35 +0900

    Update: 管理者用ルーティングを修正。

--- config/routes.rb
+++ config/routes.rb
@@ -47,6 +47,7 @@ Rails.application.routes.draw do
     resources :categories, only: [:show]
   end
   namespace :admins do
-    resources :users, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :destroy] # indexは検索結果画面
+    resources :posts, only: [:show, :destroy]
   end
 end
