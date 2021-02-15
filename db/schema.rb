7f15e3eec83e310a1e512efa520ebd125eec9955
ultrakeisuke <aucwm605@gmail.com>
2021-02-16 07:10:45 +0900

    Change: adminsテーブルで使用していないカラムを削除し、usersテーブルからadminカラムを削除。

--- db/schema.rb
+++ db/schema.rb
@@ -18,17 +18,9 @@ ActiveRecord::Schema.define(version: 2021_02_08_130131) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "answers", force: :cascade do |t|
@@ -150,7 +142,6 @@ ActiveRecord::Schema.define(version: 2021_02_08_130131) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
-    t.boolean "admin", default: false
     t.datetime "discarded_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["discarded_at"], name: "index_users_on_discarded_at"
