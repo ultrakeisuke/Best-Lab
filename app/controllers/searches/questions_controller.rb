36c350b424f2c3df59713e4d1ffda888c9db8500
ultrakeisuke <aucwm605@gmail.com>
2021-02-17 21:27:58 +0900

    Add: 管理者用ページにページネーション設定を追加。

--- app/controllers/searches/questions_controller.rb
+++ app/controllers/searches/questions_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 class Searches::QuestionsController < ApplicationController
 
+  PER = 10
+
   # 質問の検索一覧を表示
   def index
-    params[:q][:content_cont_any] = params[:q][:content_cont_any].split(/[[:blank:]]+/)
-    @posts = Post.ransack(params[:q]).result
+    params[:q][:content_cont_any] = params[:q][:content_cont_any].split(/[[:blank:]]+/) if params[:q][:content_cont_any].present?
+    @posts = Post.ransack(params[:q]).result.page(params[:page]).per(PER)
   end
 
   # 質問の検索フォームで親カテゴリーを選択した際に子カテゴリーを動的に変化させる処理
