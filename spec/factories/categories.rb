291b8be388c88541ac94aa7b95cb0762014162ca
ultrakeisuke <aucwm605@gmail.com>
2021-02-23 17:59:51 +0900

    Add: テスト用に複数の子カテゴリーを追加。

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,8 +1,18 @@
 FactoryBot.define do
+
+  # 親カテゴリー
   factory :parent_category, class: Category do
     name { "parent" }
   end
+
+  # 子カテゴリー
   factory :children_category, class: Category do
     name { "children" }
   end
+
+  # 複数の子カテゴリー
+  factory :children_categories, class: Category do
+    sequence(:name) { |n| "children#{n}" }
+  end
+
 end
