ded5f1ee2acc694a59b39a131e66c273c23bccad
ultrakeisuke <aucwm605@gmail.com>
2021-02-23 17:55:43 +0900

    Clean: system spec用のmoduleの表記を改修。
    処理内容は変わっていません。

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,21 +1,19 @@
 module LoginMacros
+
+  # 一般ユーザーのログイン
   def login_as_user(user)
     visit sign_in_path
     fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: '1234567'
+    fill_in 'パスワード', with: user.password
     click_button 'ログイン'
-    expect(page).to have_content 'ログインしました。'
   end
 
+  # 管理者のログイン
   def login_as_admin(admin)
     visit new_admin_session_path
     fill_in 'メールアドレス', with: admin.email
-    fill_in 'パスワード', with: '1234567'
+    fill_in 'パスワード', with: admin.password
     click_button 'ログイン'
   end
 
-  def logout
-    click_link 'ログアウト'
-    expect(page).to have_content 'ログアウトしました。'
-  end
 end
--- spec/support/visit_url_macros.rb
+++ spec/support/visit_url_macros.rb
@@ -1,12 +1,16 @@
 module VisitUrlMacros
+
+  # 本人確認用に送信したメールから本文からurlを抽出
   def extract_url(mail)
     body = mail.body.encoded
     body[/http[^"]+/]
   end
 
+  # 抽出したurlに移動
   def visit_url_in_mail
     mail = ActionMailer::Base.deliveries.last
     url = extract_url(mail)
     visit url
   end
+
 end
