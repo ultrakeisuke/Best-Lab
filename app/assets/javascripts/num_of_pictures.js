508874e0d1d886d7d0d079f494652125ddb14f3c
ultrakeisuke <aucwm605@gmail.com>
2021-02-28 18:59:01 +0900

    Change: eslintによるjsファイルの静的解析。

--- app/assets/javascripts/num_of_pictures.js
+++ app/assets/javascripts/num_of_pictures.js
@@ -3,7 +3,9 @@ $(function () {
   // 選択した画像の枚数をカメラアイコンの代わりに表示する処理
   $('#picture-file').change(function () {
     var files = this.files;
-    $('#picture-file-icon').replaceWith(`<i id="num-of-files">${files.length}</i>`);
+    $('#picture-file-icon').replaceWith(
+      `<i id="num-of-files">${files.length}</i>`
+    );
     if (files.length > 4) {
       $('#num-of-files').addClass('too-many-files');
     }
@@ -11,7 +13,9 @@ $(function () {
     if ($('#num-of-files')) {
       $('#picture-file').change(function () {
         var files = this.files;
-        $('#num-of-files').replaceWith(`<i id="num-of-files">${files.length}</i>`);
+        $('#num-of-files').replaceWith(
+          `<i id="num-of-files">${files.length}</i>`
+        );
         if (files.length > 4) {
           $('#num-of-files').addClass('too-many-files');
         }
--- app/assets/javascripts/user_profile.js
+++ app/assets/javascripts/user_profile.js
@@ -2,12 +2,12 @@
 window.addEventListener('load', function () {
   var affiliation = document.getElementById('user-affiliation');
   var lists = [
-    { ja: "大学生", en: "undergraduate" },
-    { ja: "大学院生", en: "graduate" },
-    { ja: "高専生", en: "technical-students" },
-    { ja: "専門学生", en: "professional-students" },
-    { ja: "社会人", en: "working" },
-    { ja: "その他", en: "other" }
+    { ja: '大学生', en: 'undergraduate' },
+    { ja: '大学院生', en: 'graduate' },
+    { ja: '高専生', en: 'technical-students' },
+    { ja: '専門学生', en: 'professional-students' },
+    { ja: '社会人', en: 'working' },
+    { ja: 'その他', en: 'other' }
   ]
   // ユーザーの所属がlistsの日本語表記と一致していれば、その英語表記のクラスを追加
   if (affiliation) {
