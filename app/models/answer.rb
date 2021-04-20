233f98e80466adadc3c36932c4b789fbb28c3b36
ultrakeisuke <aucwm605@gmail.com>
2021-04-20 22:14:48 +0900

    Fix: 質問に回答した際に回答者の通知レコードが作成できていない不具合を修正。

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,10 +6,12 @@ class Answer < ApplicationRecord
 
   # 質問に回答した際の通知処理
   def send_notice_to_questioner_or_answerers
+    # 回答時に通知レコードがなければ作成(先にリプライしていた場合は通知レコードが存在するため)
+    QuestionEntry.find_or_create(user_id: user_id, post_id: post_id)
     if user_id == post.user_id # 自己解決した場合は回答者に通知を送信
       post.send_notice_to_answerers
     else # 自己解決でない場合は質問者に通知を送信
-      questioner = QuestionEntry.find_or_create_by(user_id: post.user_id, post_id: post_id)
+      questioner = QuestionEntry.find_by(user_id: post.user_id, post_id: post_id)
       questioner.update(notice: true) unless questioner.notice
     end
   end
