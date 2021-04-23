d3603274ce3db327b7e22d28a22b383dd0802ef5
ultrakeisuke <aucwm605@gmail.com>
2021-04-24 00:28:35 +0900

    Add: リプライの投稿・編集時の画像枚数制限用のデザインを追加。

--- app/assets/javascripts/num_of_pictures.js
+++ app/assets/javascripts/num_of_pictures.js
@@ -31,3 +31,61 @@ $(function () {
     $('#picture-file-text').replaceWith(`<p>画像が選択されました</p>`);
   });
 });
+
+// リプライ作成時の画像選択に関する処理
+$(function () {
+  $(document).on('click', '.picture-file__new-reply', function () {
+    var answerID = this.getAttribute('data-picture-file__new-reply');
+    // 選択した画像の枚数をカメラアイコンの代わりに表示する処理
+    $('#picture-file__new-reply-' + answerID).change(function () {
+      var files = this.files;
+      $('#picture-file-icon__new-reply-' + answerID).replaceWith(
+        `<p id="num-of-files">${files.length}</p>`
+      );
+      if (files.length > 4) {
+        $('#num-of-files').addClass('too-many-files');
+      }
+      // 画像の再選択に関する処理
+      if ($('#num-of-files')) {
+        $('#picture-file__new-reply-' + answerID).change(function () {
+          var files = this.files;
+          $('#num-of-files').replaceWith(
+            `<p id="num-of-files">${files.length}</p>`
+          );
+          if (files.length > 4) {
+            $('#num-of-files').addClass('too-many-files');
+          }
+        });
+      }
+    });
+  });
+});
+
+// リプライ編集時の画像選択に関する処理
+$(function () {
+  $(document).on('click', '.picture-file__edit-reply', function () {
+    var replyID = this.getAttribute('data-picture-file__edit-reply');
+    // 選択した画像の枚数をカメラアイコンの代わりに表示する処理
+    $('#picture-file__edit-reply-' + replyID).change(function () {
+      var files = this.files;
+      $('#picture-file-icon__edit-reply-' + replyID).replaceWith(
+        `<p id="num-of-files">${files.length}</p>`
+      );
+      if (files.length > 4) {
+        $('#num-of-files').addClass('too-many-files');
+      }
+      // 画像の再選択に関する処理
+      if ($('#num-of-files')) {
+        $('#picture-file__edit-reply-' + replyID).change(function () {
+          var files = this.files;
+          $('#num-of-files').replaceWith(
+            `<p id="num-of-files">${files.length}</p>`
+          );
+          if (files.length > 4) {
+            $('#num-of-files').addClass('too-many-files');
+          }
+        });
+      }
+    });
+  });
+});
