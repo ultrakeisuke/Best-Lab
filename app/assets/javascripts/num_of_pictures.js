5ffe7f8faa97a4efc4c875f30a97e2919a3a0499
ultrakeisuke <aucwm605@gmail.com>
2021-04-23 19:50:52 +0900

    Add: 画像選択枚数に応じてフィールドのデザインが変更する仕様を一部に追加。
    質問投稿、質問への回答、アカウント画像の変更時が対象です。

--- app/assets/javascripts/num_of_pictures.js
+++ app/assets/javascripts/num_of_pictures.js
@@ -1,10 +1,10 @@
-// 画像選択に関する処理
+// DM、質問投稿、回答投稿時の画像選択に関する処理
 $(function () {
   // 選択した画像の枚数をカメラアイコンの代わりに表示する処理
   $('#picture-file').change(function () {
     var files = this.files;
     $('#picture-file-icon').replaceWith(
-      `<i id="num-of-files">${files.length}</i>`
+      `<p id="num-of-files">${files.length}</p>`
     );
     if (files.length > 4) {
       $('#num-of-files').addClass('too-many-files');
@@ -14,12 +14,20 @@ $(function () {
       $('#picture-file').change(function () {
         var files = this.files;
         $('#num-of-files').replaceWith(
-          `<i id="num-of-files">${files.length}</i>`
+          `<p id="num-of-files">${files.length}</p>`
         );
         if (files.length > 4) {
           $('#num-of-files').addClass('too-many-files');
         }
       });
     }
+  });
+});
+
+// プロフィール画像選択に関する処理
+$(function () {
+  // 選択した画像の枚数をカメラアイコンの代わりに表示する処理
+  $('#setting-picture-for-account').change(function () {
+    $('#picture-file-text').replaceWith(`<p>画像が選択されました</p>`);
   });
 });
