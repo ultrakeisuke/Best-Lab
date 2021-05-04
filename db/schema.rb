c43f2f064b8d1c5060add0aea5373bbf361a8c24
ultrakeisuke <aucwm605@gmail.com>
2021-05-04 16:56:50 +0900

    Change: Adminモデルからremember_created_atカラムを削除。

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20_210_208_130_131) do
   create_table 'admins', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
-    t.datetime 'remember_created_at'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.index ['email'], name: 'index_admins_on_email', unique: true
