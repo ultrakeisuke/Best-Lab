bd888c45729c3885af961d744eeb37ab794bab89
ultrakeisuke <aucwm605@gmail.com>
2021-05-07 16:01:32 +0900

    Change: ユーザーの所属が「未選択」の場合は全検索するよう変更。

--- app/controllers/searches/users_controller.rb
+++ app/controllers/searches/users_controller.rb
@@ -8,6 +8,8 @@ class Searches::UsersController < ApplicationController
     if params[:q][:description_cont_any].present?
       params[:q][:description_cont_any] = params[:q][:description_cont_any].split(/[[:blank:]]+/)
     end
+    # 所属が未設定の場合は全検索とする
+    params[:q][:affiliation_eq].clear if params[:q][:affiliation_eq] == '未設定'
     @profiles = Profile.ransack(params[:q]).result.page(params[:page])
   end
 end
--- spec/controllers/searches/users_controller_spec.rb
+++ spec/controllers/searches/users_controller_spec.rb
@@ -2,9 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Searches::UsersController, type: :request do
   let(:user) { create(:user) }
+  let(:another_user) { create(:another_user) }
   let!(:profile) { create(:profile, user_id: user.id) }
   let!(:parent_category) { create(:parent_category) }
   let!(:children_category) { create(:children_category, ancestry: parent_category.id) }
+
+  describe '所属が未設定のときの挙動について' do
+    let!(:another_profile) { create(:profile, affiliation: 'graduate', user_id: another_user.id) }
+    it '所属にかかわらず、すべてのユーザーを表示する' do
+      login_user(user)
+      get searches_users_path, params: { q: { affiliation_eq: '未設定',
+                                              school_eq: '',
+                                              faculty_eq: '',
+                                              department_eq: '',
+                                              laboratory_eq: '',
+                                              description_cont_any: '' } }
+      expect(response).to have_http_status '200'
+      expect(response.body).to include profile.user.name.to_s
+      expect(response.body).to include another_profile.user.name.to_s
+    end
+  end
 
   describe 'contentカラムに複数のキーワードを入力したときの挙動について' do
     context 'データベースに存在しないキーワードを入力した場合' do
